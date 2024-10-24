0c29d66b6f4b595e79017202dc29524b9ea61d69
Kite Tasx <kitetasxpete@gmail.com>
2024-10-24 17:49:00 -0500

    Se agrega el archivo

---
+++ estructuradedatosProject/Listas.py
@@ -0,0 +1,12 @@
+#Listas
+lista = []#Definir lista vacia
+print(lista)
+lista1 = [1, 2,3,4,5, 'hola',4.5] #Una lista heterogenea
+print(lista1)
+
+#Enlazar las listas
+lista2 =[0,1,2,3]
+lista3 = ["A","B","C"]
+lista4 = [lista2, lista3]
+print(lista4)
+print(lista4[1][1])
